26afb9cd64841b31fb2041bae634392bfdcef89e
mw483 <wijaya.m.ab@m.titech.ac.jp>
2025-04-15 21:18:19 +0900

    basic functions

---
+++ calculator.js
@@ -0,0 +1,16 @@
+function add (a, b) {
+    return result = a + b
+}
+
+function subtract (a, b) {
+    return result = a - b
+}
+
+function multiply (a, b) {
+    return result = a * b
+}
+
+function divide (a, b) {
+    return result = a / b
+}
+
