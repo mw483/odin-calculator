fbe3e788d6d9ba731f1a5896087885f5786ee15e
mw483 <wijaya.m.ab@m.titech.ac.jp>
2025-04-17 12:28:51 +0900

    operate fn, button layout prep

--- calculator.js
+++ calculator.js
@@ -1,16 +1,37 @@
-function add (a, b) {
-    return result = a + b
+let firstNumber
+let secondNumber
+let result
+let operator
+
+function add (firstNumber, secondNumber) {
+    return result = firstNumber + secondNumber
 }
 
-function subtract (a, b) {
-    return result = a - b
+function subtract (firstNumber, secondNumber) {
+    return result = firstNumber - secondNumber
 }
 
-function multiply (a, b) {
-    return result = a * b
+function multiply (firstNumber, secondNumber) {
+    return result = firstNumber * secondNumber
 }
 
-function divide (a, b) {
-    return result = a / b
+function divide (firstNumber, secondNumber) {
+    return result = firstNumber / secondNumber
 }
 
+function operate (firstNumber, secondNumber) {
+    if (operator === "add") {
+        add(firstNumber, secondNumber)
+    }
+    else if (operator === "subtract") {
+        subtract(firstNumber, secondNumber)
+    }
+    else if (operator === "multiply") {
+        multiply(firstNumber, secondNumber)
+    }
+    else if (operator === "divide") {
+        divide(firstNumber, secondNumber)
+    }
+    console.log(result)
+}
+ 
